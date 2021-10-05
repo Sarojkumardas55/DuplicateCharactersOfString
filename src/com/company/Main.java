d463acd0cedb04a98f22a3e6a52e04d9168b2163
Saroj Das <sarojkumardas55@gmail.com>
2021-10-04 22:18:05 -0700

    changes in varriable and function name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,19 @@ import java.util.HashMap;
 import java.util.Set;
 
 class DuplicateCharactersFromString{
-     static void duplicateCharCount(String inputString){
+     static void duplicateCharFind(String input){
         HashMap<Character,Integer> charCountMap=new HashMap<Character,Integer>();
-        char[]strArray=inputString.toCharArray();
+        char[]strArray=input.toCharArray();
         for (char c:strArray){
             if(charCountMap.containsKey(c)){
                 charCountMap.put(c,charCountMap.get(c)+1);
             }
             else {
-
                 charCountMap.put(c,1);
             }
-
         }
         Set <Character> charsInString=charCountMap.keySet();
-        System.out.println("Duplicate Characters In "+ inputString);
+        System.out.println("Duplicate Characters In "+ input);
         for (Character ch : charsInString){
             if (charCountMap.get(ch)>1){
                 System.out.println("character- "+ch+" duplicates :"+charCountMap.get(ch));
@@ -32,9 +30,9 @@ class DuplicateCharactersFromString{
 public class Main {
 
     public static void main(String[] args) {
-        DuplicateCharactersFromString.duplicateCharCount("Saroj");
-        DuplicateCharactersFromString.duplicateCharCount("sarojdas");
-        DuplicateCharactersFromString.duplicateCharCount("dingdong");
+        DuplicateCharactersFromString.duplicateCharFind("Saroj");
+        DuplicateCharactersFromString.duplicateCharFind("sarojdas");
+        DuplicateCharactersFromString.duplicateCharFind("dingdong");
 	// write your code here
     }
 }
